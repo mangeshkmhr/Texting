c4bcac502c50fef4d84d51fdc748560d6a8e3fcb
mangeshkmhr <mangeshkumhar@gmail.com>
2022-06-01 19:33:50 +0530

    Updates

---
+++ precache-manifest.8c8de63fe18aab495a8d161c25cfab76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34d717ab7224cff0aaea",
+    "url": "/Texting/static/css/main.9516a037.chunk.css"
+  },
+  {
+    "revision": "34d717ab7224cff0aaea",
+    "url": "/Texting/static/js/main.34d717ab.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Texting/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/Texting/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "723bd230dab08a79b376",
+    "url": "/Texting/static/js/runtime~main.723bd230.js"
+  },
+  {
+    "revision": "f2f4767f72b0bef2bcd4c48d17ed1a7a",
+    "url": "/Texting/index.html"
+  }
+];
